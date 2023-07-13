394f5b36ccd9cc4ba9f01432513c4f079cfa60ca
Maicol Jaquez <maicol120205@gmail.com>
2023-07-13 04:46:09 +0100

    API added to proyect

---
+++ API/Controllers/AnnouncementController.cs
@@ -0,0 +1,34 @@
+﻿using Api.Data;
+using API.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnnouncementController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public AnnouncementController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<AnnouncementController>
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var announcement = await _context.Announcement.ToListAsync();
+            return Ok(announcement);
+        }
+
+      
+        
+    }
+}
